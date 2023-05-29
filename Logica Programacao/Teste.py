d8d3ab0e7dfea6faded48620e32f012b12d77abc
Marcus Viniccius <73945203+mviniccius@users.noreply.github.com>
2023-05-29 12:36:13 -0300

    Add files via upload

--- Logica Programacao/Teste.py
+++ Logica Programacao/Teste.py
@@ -7,24 +7,27 @@ while True:
     print("4 - Subtrair")
     print("5 - Sair do programa")
     menu = int(input("Digite a opcao desejada: "))    
-    if menu == 5:
-        break
     
-    num1 = int(input("Digite o primeiro valor: "))
-    num2 = int(input("Digite o degundo valor: "))
-    
+    if menu < 1 or menu > 5:
+        print("Opcao invalida!")
+    else:
+        if menu == 5:
+            break
+        else:
+            num1 = int(input("Digite o primeiro valor: "))
+            num2 = int(input("Digite o degundo valor: "))        
 
-    if menu == 1:
-        soma = num1 + num2
-        print("O resultado da soma = ", soma)
-    if menu == 2:
-        multiplicar = num1 * num2
-        print("O resultado da soma = ", multiplicar)
-    if menu == 3:
-        dividir = num1 / num2 
-        print("O resultado da divisao = ", dividir)
-    if menu == 4:
-        subtrair = num1 - num2
-        print("O resultado da subtracao = ", subtrair)
+            if menu == 1:
+                soma = num1 + num2
+                print("O resultado da soma = ", soma)
+            if menu == 2:
+                multiplicar = num1 * num2
+                print("O resultado da soma = ", multiplicar)
+            if menu == 3:
+                dividir = num1 / num2 
+                print("O resultado da divisao = ", dividir)
+            if menu == 4:
+                subtrair = num1 - num2
+                print("O resultado da subtracao = ", subtrair)
 
 print("Obrigado por utilizar!")
