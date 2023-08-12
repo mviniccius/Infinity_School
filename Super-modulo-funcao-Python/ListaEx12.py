516ae933e5a7c120cae3d7d565ec361a8da20a41
mviniccius <marcus.viniccius@hotmail.com>
2023-08-12 14:19:28 -0300

    exercicio 12

---
+++ Super-modulo-funcao-Python/ListaEx12.py
@@ -0,0 +1,15 @@
+# Faça uma função que recebe uma lista de números e retorna a média deles. 
+# Exemplo de entrada: [4, 5, 6, 7] 
+# Saída esperada: 5.5
+
+def calcula_media(numeros: list) -> float:
+    quantidade = len(numeros)
+    soma = 0
+    for i in numeros:
+        soma +=i
+    
+    media = soma / quantidade
+    return media
+
+lista_num = [8, 15, 23, 4]
+print(calcula_media(lista_num))
