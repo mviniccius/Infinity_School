809e8e6f99fd997bfc1dde0e8b66ec10bf51fad6
mviniccius <marcus.viniccius@hotmail.com>
2023-09-11 16:03:09 -0300

    pequena alteracao

--- Curso-FullStack/POO 2608/calculadora.py
+++ Curso-FullStack/POO 2608/calculadora.py
@@ -2,7 +2,7 @@ class Calculadora:
     def __init__(self, num1:int, num2:int) -> None:
         self.num1 = num1
         self.num2 = num2
-        pass
+        
     
     
     def soma_numeros(self) -> int:
