302dab97f8caac0cc9202b0e184e55c53a65d10f
mviniccius <marcus.viniccius@hotmail.com>
2023-10-02 21:11:48 -0300

    criando menu de utilizacao

--- Curso-FullStack/Aula 30 09/Aula16 CRUD/main.py
+++ Curso-FullStack/Aula 30 09/Aula16 CRUD/main.py
@@ -2,4 +2,32 @@ from crud import CRUD
 
 registro = CRUD('db.db', 'admin', 'admin')
 
-registro.criar('ana', 10)
+menu = True
+while menu:
+    
+    print("0 - Sair")
+    print("1 - Cadastrar")
+    print("2 - Imprimir lista")
+    print("3 - Atualizar cadastro - TOTAL")
+    print("4 - Deletar por ID", end="\n\n")
+    controle = input("Escolha a opcao desejada!\n")
+
+    if controle == '1':
+        print("Entrou")
+
+    elif controle == '2':
+        print("Entrou")
+
+    elif controle == '3':
+        print("Entrou")
+    
+    elif controle == '4':
+        print("Entrou")
+        
+    elif controle == '0':
+        menu = False
+    
+    else:
+        print("Opcao invalida")
+    
+print("Obrigado por utilizar o sistema")
